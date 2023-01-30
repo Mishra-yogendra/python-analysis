961f066d0bfb4549ebf5d393cea1cf206fd2f84f
yogendra mishra <mishrayogendra2002@gmail.com>
2023-01-30 11:33:04 +0530

    first commit

---
+++ hyp.py
@@ -0,0 +1,9 @@
+per = 12
+base = 5
+
+hyp = (per** + base**2)**.5
+print("PYTHOGOREAN THEOREM")
+print(f"perpendicular is {per}")
+print(f"base is {base}")
+print("==" *10)
+print("hypotenuse is ", hyp)
---
+++ si.py
@@ -0,0 +1,10 @@
+#SI calculator
+p=10000
+r=0.5
+t=10
+s=p*r*t/100
+print(f"principe is ${p}")
+print(f"rate is {r}%")
+print(f"time is {t}yrs")
+print('__'*10)
+print(f"Simple Interest is {s}")

2f495d75da48688d021987bf610d27492f296482
yogendra mishra <mishrayogendra2002@gmail.com>
2023-02-18 12:34:16 +0530

    game

---
+++ game.py
@@ -0,0 +1,22 @@
+import pgzrun
+
+music.play('bgm')
+music.set_volume(0.5)
+b = Rect((50,50),(50,25))
+vx,vy = 3,2
+
+def draw():
+    screen.fill('white')
+    screen.draw.filled_rect(b, 'blue')
+
+def update():
+    global vx,vy
+    b.x += vx
+    b.y += vy
+    if b.right > 800 or b.left < 0:
+        vx = -vx
+        sounds.s1.play()
+    if b.bottom > 600 or b.top < 0:
+        vy = -vy
+        sounds.s1.play
+pgzrun.go()
